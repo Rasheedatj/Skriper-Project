1f183c15d08783d638c68fc97da81c03597145f9
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2023-03-13 01:55:36 +0100

    completed faq

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const loginForm = document.querySelector(".login");
 loginBtn.addEventListener("click", () => {
   loginForm.classList.toggle("active");
 });
+
+let faq = document.querySelectorAll(".faq div");
+
+faq.forEach((question) => {
+  question.addEventListener("click", () => {
+    question.classList.toggle("open");
+  });
+});
